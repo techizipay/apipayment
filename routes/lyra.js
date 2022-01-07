3184a1878246fa3937347dbebb6fc870b39977c3
Tech Izipay <techizipay@gmail.com>
2022-01-07 16:03:18 -0500

    Otra version

--- routes/lyra.js
+++ routes/lyra.js
@@ -3,7 +3,6 @@ const lyraController = require('../controller/Lyra');
 
 var lyra_router = Router();
 lyra_router.post('/createpayment', lyraController.CreatePayment); 
-lyra_router.post('/createpaymentaut', lyraController.CreatePaymentAut); 
-lyra_router.post('/validarhash', lyraController.validarHash); 
+lyra_router.post('/createpaymentaut', lyraController.CreatePaymentAut);  
 lyra_router.post('/obteneripn', lyraController.ReceiveIpn); 
 module.exports = lyra_router;
